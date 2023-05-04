9b23c4fe927286a24594c87286f2d04267bde5ed
Asus <Asus@LAPTOP-0MGGJ01R>
2023-05-04 11:35:16 +0700

    Add project files.

---
+++ WebApplication2/Databases/DataDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore; //Import EF
+using WebApplication2.Models;
+
+namespace WebApplication2.Databases
+{
+    public class DataDbContext : DbContext
+    {
+        // Constructure Method
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) { }
+        //Table manufacturers
+        //Table devices
+        public DbSet<positions> positions { get; set; }
+
+        public DbSet<employees> employees { get; set; }
+    }
+}
